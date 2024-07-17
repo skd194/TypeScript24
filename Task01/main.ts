ef07036d619ff199db2d79f615573d038055b45c
sks <shyamkumarsb007@gmail.com>
2024-07-17 05:46:24 +0530

    initial setup

---
+++ Task01/main.ts
@@ -0,0 +1,12 @@
+let age: number = 20;
+
+
+function doSomething() {
+    for (let i = 0; i < 5; i++) {
+        console.log();
+        console.log("Finally " + i);
+    }
+
+}
+
+doSomething();
---
+++ Task01/out/main.js
@@ -0,0 +1,8 @@
+var age = 20;
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log();
+        console.log("Finally " + i);
+    }
+}
+doSomething();
