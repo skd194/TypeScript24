0511506e999957795ac2d5de973d10c70f39d52b
sks <shyamkumarsb007@gmail.com>
2024-07-18 08:47:50 +0530

    advanced types completed

--- src/index.ts
+++ src/index.ts
@@ -68,4 +68,106 @@ user.name = 'Davis';
 
 let d = { id: 'number' }
 
+// type alias
+type User = {
+    readonly id: number,
+    name: string,
+    displayName: (moduleId: number) => void
+}
 
+// Union types
+function celsiusToKelvin(temperature: number | string): number {
+    // Narrowing
+    if (typeof temperature === 'number') {
+        return temperature + 273
+    }
+    return parseInt(temperature) + 273
+}
+
+
+// Intersection Types
+type Draggable = {
+    drag: () => void
+}
+
+type Resizable = {
+    resize: () => void
+}
+
+type UIWidget = Draggable & Resizable
+
+let textBox: UIWidget = {
+    drag() { },
+    resize() { },
+}
+
+// literal types
+type Quantity = 50 | 100;
+let quantity: Quantity = 100
+
+type Metric = 'cm' | 'm'
+
+// optional chaining
+type Customer = {
+    birthday: Date
+}
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(0);
+
+// Optional Property access operator
+console.log(customer?.birthday?.getFullYear())
+
+// Optional element access operator 
+// Customer?.[0]
+
+// Optional call 
+let log: any = null;
+log?.('logContent')
+
+// Nullish Coalescing operator
+let speed = null;
+let velocity = speed ?? 0;
+
+// Type Assertions
+let phone1 = document.getElementById('phone') as HTMLInputElement;
+// or 
+let phone2 = <HTMLInputElement>document.getElementById('phone');
+// no casting happens here like other languages like C#, just informing the compiler 
+// that this expression is of the specified type, which inturn bring the right intellisense
+
+phone1.value;
+phone2.value;
+
+// unknown
+function render(document: unknown) {
+    // Narrowing
+    if (typeof document === 'string') {
+        document.toLowerCase();
+    }
+    if (document instanceof HTMLInputElement) {
+        console.log(document.value)
+    }
+    // document.move();
+    // document.fly();
+    // document.something();
+}
+
+// Use unknown instead of any type bcz the compiler forces us to do some type checking
+// to make sure the methods we are calling exists in the target object. 
+// -------------
+
+
+// never
+function processEvents(): never {
+    throw new Error("error")
+    // while (true) {
+
+    // }
+}
+
+processEvents();
+console.log("hello")
